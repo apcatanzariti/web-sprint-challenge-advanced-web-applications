6a6a295dc391dc64483200a17a923c7c57ad1e47
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-02-26 19:54:41 -0500

    mvp finished except for one of the tests

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -3,34 +3,28 @@ import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 import { fetchColors as mockFetchColors } from './../api/fetchColors';
 
-jest.mock('./../api/fetchColors');
-
-const colorsData = [
-  {color:"aliceblue",code:{hex:"#f0f8ff"},id:1},
-  {color:"limegreen",code:{hex:"#99ddbc"},id:2},
-  {color:"aqua",code:{hex:"#00ffff"},id:3},
-  {color:"aquamarine",code:{hex:"#7fffd4"},id:4},
-  {color:"lilac",code:{hex:"#9a99dd"},id:5},
-  {color:"softpink",code:{hex:"#dd99ba"},id:6},
-  {color:"bisque",code:{hex:"#dd9a99"},id:7},
-  {color:"softyellow",code:{hex:"#dcdd99"},id:8},
-  {color:"blanchedalmond",code:{hex:"#ffebcd"},id:9},
-  {color:"blue",code:{hex:"#6093ca"},id:10},
-  {color:"blueviolet",code:{hex:"#8a2be2"},id:11}
-  ];
-
 test("Renders BubblePage without errors", () => {
   render(<BubblePage />);
 });
 
+jest.mock('./../api/fetchColors');
+
+const colorsData = {
+    color:"aqua",
+    code:{hex:"#00ffff"},
+    id:3
+  };
+
 test("Fetches data and renders the bubbles on mounting", async () => {
+  
   mockFetchColors.mockResolvedValueOnce(colorsData);
 
-  const { debug } = render(<BubblePage />);
+  render(<BubblePage />);
 
-  expect(await screen.findByText(/aqua/i).toBeInTheDocument());
-  
-  debug();
+  const colorSpan = await screen.findByText(/aqua/i);
+
+  expect(colorSpan).toBeInTheDocument();
+
 });
 
 //Task List
